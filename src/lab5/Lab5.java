d3a2d1e034bf132afd10e17177b323c6983023ca
phungle2508 <shegga9x@gmail.com>
2026-04-17 07:57:08 +0700

    lab5

---
+++ src/lab5/Lab5.java
@@ -0,0 +1,41 @@
+package lab5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab5 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		int number = 0; 
+		int randomNumber = random.nextInt(100) + 1; 
+		boolean flag = true;
+		int numTries = 0;
+		
+		while (flag) {
+			System.out.println("Please enter a number from 0 to 100 (-1 to exit):");
+			number = scanner.nextInt(); 
+			if (number == -1) {
+				flag = false;
+			} else if (number < 0 || number > 100) {
+				System.out.println("Please enter a number from 0 to 100");
+			} else {
+				number++;
+				if (number > randomNumber) {
+					System.out.println("Please enter a smaller number");
+				} else if (number < randomNumber) {
+					System.out.println("Please enter a bigger number");
+				} else {
+					flag = false;
+                    System.out.println("Congratulations! You've found the number " + number + " in " + numTries + " tries.");
+				}
+			}
+		}
+
+		System.out.println("Exiting program...");
+		scanner.close();
+	}
+
+}
